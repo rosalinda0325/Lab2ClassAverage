8ba82f81727e27113e1ee90e1f2dffb3ef7d7879
rosalinda0325 <rosalinda03@mail.fresnostate.edu>
2021-05-14 06:51:22 -0700

    first commit

---
+++ lab2_class_average.py
@@ -0,0 +1,14 @@
+
+"""
+This program will calculate the input of class average of the test scores
+It will start(counter) with a accumlator(sum) and set it to zero
+loop through the list of test scores
+add all test scores
+then increment or add by 1
+as the loop ends there are no more scores to add
+divide the scores(sum) by the number of test scores(counter)
+print the output to the user
+
+output will print the average of the class scores
+
+"""
